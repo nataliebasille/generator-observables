6b25d634f16c20b787682a5d73170b6c7cc82b69
Natalie Basille <natalie.basille1@gmail.com>
2024-12-26 19:03:44 -0500

    fix: export missing functions

--- src/streams/index.ts
+++ src/streams/index.ts
@@ -3,5 +3,6 @@ export * from './empty/empty';
 export * from './from/from';
 export * from './interval/interval';
 export * from './merge/merge';
-export * from './zip/zip';
+export * from './share/share';
 export * from './stream';
+export * from './zip/zip';
--- src/terminators/index.ts
+++ src/terminators/index.ts
@@ -1,4 +1,5 @@
 export * from './collect/collect';
 export * from './count/count';
+export * from './find/find';
 export * from './last/last';
 export * from './terminator';
